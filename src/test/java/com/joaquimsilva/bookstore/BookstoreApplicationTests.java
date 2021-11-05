934f615f336c282535fadf9d481b4f9de354ccfd
Joaquim Silva <joaquim.silva@reservafacil.tur.br>
2021-11-04 21:35:38 -0300

    first commit: created struct

---
+++ src/test/java/com/joaquimsilva/bookstore/BookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaquimsilva.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
